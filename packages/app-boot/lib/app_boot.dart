b09607773da76a570ac5e7bd0672b923502e4152
Marco Coppola <ingcoppolamarco@gmail.com>
2022-10-04 11:17:46 +0200

    Version 0.0.1

--- packages/app-boot/lib/app_boot.dart
+++ packages/app-boot/lib/app_boot.dart
@@ -1,6 +1,54 @@
 library app_boot;
 
-/// A Calculator.
-class Calculator {
-  void foo() => print('Works');
+import 'package:flutter/material.dart';
+
+class AppBoot extends StatefulWidget {
+  const AppBoot({
+    Key? key,
+    required this.splashScreen,
+    required this.initializeApp,
+    required this.onDone,
+    required this.onError,
+  }) : super(key: key);
+
+  final Widget splashScreen;
+  final Future<void> initializeApp;
+  final VoidCallback onDone;
+  final VoidCallback onError;
+
+  @override
+  State<AppBoot> createState() => _AppBootState();
+}
+
+class _AppBootState extends State<AppBoot> {
+  // **************************************************************************
+  // LIFE CYCLE
+  // **************************************************************************
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  // **************************************************************************
+  // BUSINESS LOGIC
+  // **************************************************************************
+  Future<void> _init() async {
+    try {
+      await widget.initializeApp;
+      widget.onDone();
+    } catch (e, s) {
+      print(e);
+      print(s);
+      widget.onError();
+    }
+  }
+
+  // **************************************************************************
+  // BUILD
+  // **************************************************************************
+  @override
+  Widget build(BuildContext context) {
+    return widget.splashScreen;
+  }
 }
